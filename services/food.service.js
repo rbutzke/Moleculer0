7b11a16bb004bf0f50258eb482113cf4c0d048a5
rbutzke <roberto.butzke.junior@gmail.com>
2021-04-21 17:08:58 -0300

    atualizacao

--- services/food.service.js
+++ services/food.service.js
@@ -3,16 +3,16 @@ const { get } = require('http');
 module.exports = {
     name:'food',
     actions: {
-        async get(){
-            const result = await new Promise((resolve, reject) => {
-                get('http://taco-food-api.herokuapp.com/api/v1/food',(res)=>{
-                    let data = ''
-                    res.on('data', (value) => (data += value))
-                    res.on('error',(error) => reject(error))
-                    res.on('end',() => resolve(JSON.parse(data)))
-                })
-              })
-              return result
+            async get(){
+              const [ foods, categories ] = await Promise.all([this.getFood(), this.getCategories()])
+
+              const result = foods.map(food => ({
+                  id: food.id,
+                  description: food.description,
+                  categoriry: categories.find(category => category.id === food.category_id).category
+              }))
+
+              return result;
             },
             async categories(){
                 return await this.getCategories()
@@ -21,6 +21,7 @@ module.exports = {
 
     methods: {
         async getCategories(){
+
             const result = await new Promise((resolve,reject)=> {
                 get('http://taco-food-api.herokuapp.com/api/v1/category',(res)=>{
                     let data= ''
@@ -28,8 +29,20 @@ module.exports = {
                     res.on('error',(error) => reject(error))
                     res.on('end',() => resolve(JSON.parse(data)))
                 })
-            })
-            return result
-        }
-    }
+              })
+              return result
+            },
+
+        async getFood(){
+            const result = await new Promise((resolve, reject) => {
+                get('http://taco-food-api.herokuapp.com/api/v1/food',(res)=>{
+                   let data = ''
+                   res.on('data', (value) => (data += value))
+                   res.on('error',(error) => reject(error))
+                   res.on('end',() => resolve(JSON.parse(data)))
+                })
+              })
+              return result
+            },    
+     }
 }
